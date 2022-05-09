9e0a2ac676e17f6ea466b40f44f3b81d5ce0c944
Maurilio Souza <1mauriliosouza@gmail.com>
2022-05-09 05:54:39 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackData from "./data/FeedbackData"
 
 function App() {
+  const [feedback, setFeedbackData] = useState(FeedbackData)
+  
   return (
     <>
       <Header />
+      <div className="container">
+        <FeedbackList feedback={feedback} />
+      </div>
     </>
   )
 }
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,32 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 9,
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum temporibus suscipit 
+    adipisci illum ullam, vel quidem assumenda voluptas dicta esse, autem non sapiente commodi 
+    tempore soluta quod, perferendis dolor laboriosam.`,
+  },
+  {
+    id: 2,
+    rating: 6,
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum temporibus suscipit 
+    adipisci illum ullam, vel quidem assumenda voluptas dicta esse, autem non sapiente commodi 
+    tempore soluta quod, perferendis dolor laboriosam.`,
+  },
+  {
+    id: 3,
+    rating: 10,
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum temporibus suscipit 
+    adipisci illum ullam, vel quidem assumenda voluptas dicta esse, autem non sapiente commodi 
+    tempore soluta quod, perferendis dolor laboriosam.`,
+  },
+  {
+    id: 4,
+    rating: 8,
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum temporibus suscipit 
+    adipisci illum ullam, vel quidem assumenda voluptas dicta esse, autem non sapiente commodi 
+    tempore soluta quod, perferendis dolor laboriosam.`,
+  },
+]
+
+export default FeedbackData
