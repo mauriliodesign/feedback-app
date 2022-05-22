a4e0d84a46dca68a4aff39f0afdc4bbd03850bd0
Maurilio Souza <1mauriliosouza@gmail.com>
2022-05-22 22:27:04 +0100

    .

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+import Card from "../components/shared/Card"
+
+function AboutPage() {
+  return <Card>
+    <div className="about">
+      <h1>About This Project</h1>
+      <p>This is a React app to leave feedbacks for a products or service</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  </Card>
+}
+
+export default AboutPage
