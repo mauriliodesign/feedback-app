581544e7dafefcf6bf36bb6becff57a51e945e72
Maurilio Souza <1mauriliosouza@gmail.com>
2022-06-03 08:35:50 +0100

    .

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,5 @@
 import Card from "../components/shared/Card"
+import { Link } from "react-router-dom"
 
 function AboutPage() {
   return <Card>
@@ -6,6 +7,7 @@ function AboutPage() {
       <h1>About This Project</h1>
       <p>This is a React app to leave feedbacks for a products or service</p>
       <p>Version: 1.0.0</p>
+      <Link to="/">Back to home</Link>
     </div>
   </Card>
 }
